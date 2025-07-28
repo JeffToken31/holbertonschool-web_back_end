784c10a59ecc9c38a2e8051e1cf3c18fd638aa26
jeff <jeffrey-31@hotmail.fr>
2025-07-28 11:46:25 +0200

    Function takes a mapping, any and union of T and any to return union of any, T

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """
-Function takes a list of sequence typing with any and
-return an optionnal containing tyoe any
+Function takes a mapping, any and union of T and any
+to return union of any, T
 """
 from typing import TypeVar, Mapping, Any, Union
 
 T = TypeVar('T')
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, Any] = None) -> Union[Any, T]:
+    """
+    Function takes a mapping, any and union of T and any
+    to return union of any, T
+    """
     if key in dct:
         return dct[key]
     else:
