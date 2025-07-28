1d89385f0f5d8df4b2fafe3d820266875c32a59f
jeff <jeffrey-31@hotmail.fr>
2025-07-28 10:34:16 +0200

    To create a variable with two type possible use union

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,10 +6,10 @@ The first element of the tuple is the string k.
 The second element is the square of the int/float
 v and k should be annotated as a float
 """
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: float | int) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Function that takes a string k and an int OR float
     v as arguments and returns a tuple.
