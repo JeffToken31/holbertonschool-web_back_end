0ed27bdea349f39decb6079f822546c3aa74dd43
jeff <jeffrey-31@hotmail.fr>
2025-07-28 09:34:07 +0200

    Concat str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Function who concat two string variables annotation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function who concat two string variables annotation
+    """
+    return str1 + str2
