a85724cd9ffb154621a7bf68e2bed03f1205e2d1
jeff <jeffrey-31@hotmail.fr>
2025-07-28 12:10:04 +0200

    i found union to factor variable

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Function takes a list, int or float
+to return list
+"""
+from typing import Union, List
+
+
+def zoom_array(lst: List, factor: Union[int, float] = 2) -> List:
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
