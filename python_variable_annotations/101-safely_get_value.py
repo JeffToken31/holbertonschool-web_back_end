5de979ce8ddf6a5bcbbbbeafa1ed1c0cef6c3ef9
jeff <jeffrey-31@hotmail.fr>
2025-07-28 11:43:19 +0200

    It's easy to typing variable but i don't understand this type

---
+++ python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function takes a list of sequence typing with any and
+return an optionnal containing tyoe any
+"""
+from typing import TypeVar, Mapping, Any, Union
+
+T = TypeVar('T')
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, Any] = None) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
