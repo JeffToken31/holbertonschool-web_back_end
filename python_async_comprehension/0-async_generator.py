3e3de1a453e0a378d0b8dca7cc413c38a77d54fd
jeff <jeffrey-31@hotmail.fr>
2025-07-29 09:33:07 +0200

    random.uniform 0 to 10

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-Function genrator yield a ramdom number wai0ingt 1 sec ten times
+Function genrator yield a ramdom number waiting 1 sec ten times
 """
 import random
 from typing import AsyncGenerator
 import asyncio
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    Function genrator yield a ramdom number wai0ingt 1 sec ten times
+    Function genrator yield a ramdom number waiting 1 sec ten times
     """
     for _ in range(10):
-        yield random.random()
         await asyncio.sleep(1)
-        
+        yield random.uniform(0, 10)
