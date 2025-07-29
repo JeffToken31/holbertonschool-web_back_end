d78064f5778cd318424a56e9d64dd7c09c9d799d
jeff <jeffrey-31@hotmail.fr>
2025-07-29 10:21:41 +0200

    function typing

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,7 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Coroutin collecte 10 random numbers with use of list comprehension
     and return list
