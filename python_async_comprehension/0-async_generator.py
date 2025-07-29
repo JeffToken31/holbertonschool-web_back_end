02de3e1b7ebf4fe0aa330c270927793570eaba5a
jeff <jeffrey-31@hotmail.fr>
2025-07-29 09:43:38 +0200

    add one more none in asyncgenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,11 +8,11 @@ from typing import AsyncGenerator
 import asyncio
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """
     Asynchronous generator that yields a random float between 0 and 10,
     after waiting 1 second, repeated 10 times.
     """
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
