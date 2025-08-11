f2b743e2d727db72922ecbb0fd2a7823eb59f663
jeff <jeffrey-31@hotmail.fr>
2025-08-11 19:26:27 +0200

    Return a list containing a specific topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Return a list containing a specific topic
+"""
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+mongo_collection = client.my_db.school
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Return a list containing a specific topic
+    """
+    result = []
+    for school in mongo_collection.find({},{"topics": topic}):
+        result.append(school)
+    return result
