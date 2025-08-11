33853463eab0f2176126352c34e0a28876672c38
jeff <jeffrey-31@hotmail.fr>
2025-08-11 19:35:24 +0200

    correction of line 15

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -12,4 +12,4 @@ def schools_by_topic(mongo_collection, topic):
     """
     Return a list containing a specific topic
     """
-    return mongo_collection.find({},{"topics": topic})
+    return mongo_collection.find({"topics": topic})
