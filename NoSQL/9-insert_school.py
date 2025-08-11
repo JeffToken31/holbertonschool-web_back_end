d91e5563f5538c1ead34fa2c1c7e3b8e0460b677
jeff <jeffrey-31@hotmail.fr>
2025-08-11 17:07:44 +0200

    Insert datas in collection and return id of this

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Fonction to list all documents in a collection
+"""
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+mongo_collection = client.my_db.school
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Insert datas in collection and return id of this
+    """
+    return mongo_collection.insert(kwargs).inserted_ids
