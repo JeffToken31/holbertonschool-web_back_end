bbe8020f5240e69647f039f2f9e13589a3944547
jeff <jeffrey-31@hotmail.fr>
2025-08-12 09:23:03 +0200

    keep methods in a loop

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -11,17 +11,11 @@ count_logs = mongo_collection.count_documents({})
 print("{} logs".format(count_logs))
 print("Methods:")
 
-get = mongo_collection.count_documents({"method": "GET"})
-print("\tmethod GET: {}".format(get))
-post = mongo_collection.count_documents({"method": "POST"})
-print("\tmethod POST: {}".format(post))
-put = mongo_collection.count_documents({"method": "PUT"})
-print("\tmethod PUT: {}".format(put))
-patch = mongo_collection.count_documents({"method": "PATCH"})
-print("\tmethod PATCH: {}".format(patch))
-delete = mongo_collection.count_documents({"method": "DELETE"})
-print("\tmethod DELETE: {}".format(delete))
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+for method in methods:
+        count_method = mongo_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count_method))
 
 count_status = mongo_collection.count_documents({"method": "GET", "path": "/status"})
 print("{} status check".format(count_status))
