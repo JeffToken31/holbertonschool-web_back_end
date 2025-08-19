4cd7ce7929daede651c11ed421367b95fd3d1143
jeff <jeffrey-31@hotmail.fr>
2025-08-19 10:01:26 +0200

    test with new config eslint

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const tmpArray = [];
-    for (let idx of array) {
-        tmpArray.push(appendString + idx);
-    }
-    return tmpArray;
+  const tmpArray = [];
+  for (let idx of array) {
+    tmpArray.push(appendString + idx);
+  }
+  return tmpArray;
 }
