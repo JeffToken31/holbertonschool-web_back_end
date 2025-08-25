fc2ba207098c931ad51a68b613117611fc85875e
jeff <jeffrey-31@hotmail.fr>
2025-08-25 15:37:31 +0200

    function that clean a set and use array to return a string

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+    if (!startString) {
+        return '';
+    }
+    let array = [];
+    set.forEach(element => {
+        if (element.slice(0, startString.length) === startString) {
+            array.push(element.slice(startString.length));
+       }
+    });
+    return array.join('-');
+}
