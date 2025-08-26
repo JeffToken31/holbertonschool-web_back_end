e5dc194ab86e41d827a0a926db993ec91619acda
jeff <jeffrey-31@hotmail.fr>
2025-08-26 13:33:31 +0200

    semistandard

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,9 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
-      .then(([rep1, rep2]) =>
-        console.log(`${rep1.body} ${rep2.firstName} ${rep2.lastName}`)
-      )
-      .catch(() => console.log('Signup system offline'));
+export default function handleProfileSignup () {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([rep1, rep2]) =>
+      console.log(`${rep1.body} ${rep2.firstName} ${rep2.lastName}`)
+    )
+    .catch(() => console.log('Signup system offline'));
 }
