f42ad5d3c1f1f40a84760bc5eaaac871dfe12029
jeff <jeffrey-31@hotmail.fr>
2025-08-26 18:09:26 +0200

    add a return in .then

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from "./5-photo-reject.js";
 export default function handleProfileSignup(firstName, lastName, filename) {
 	return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
 		.then((responses) => {
-			responses.map((response) => ({
+			return responses.map((response) => ({
 				status: response.status,
 				value: response.value || response.reason,
 			}))
