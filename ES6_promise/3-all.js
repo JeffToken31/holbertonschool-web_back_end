7e3e3d481dd23577dc08d5bb92b7ec9befff2f65
jeff <jeffrey-31@hotmail.fr>
2025-08-26 13:38:40 +0200

    call function inverted

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup () {
-  Promise.all([uploadPhoto(), createUser()])
+  Promise.all([createUser(), uploadPhoto()])
     .then(([rep1, rep2]) =>
       console.log(`${rep1.body} ${rep2.firstName} ${rep2.lastName}`)
     )
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => console.log('Signup system offline'))
 }
