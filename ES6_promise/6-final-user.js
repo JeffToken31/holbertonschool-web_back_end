c020eea966bcd166441bd14584a901944d1a124a
jeff <jeffrey-31@hotmail.fr>
2025-08-26 18:15:05 +0200

    pop return in then

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from "./5-photo-reject.js";
 export default function handleProfileSignup(firstName, lastName, filename) {
 	return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
 		.then((responses) => {
-			return responses.map((response) => ({
+			responses.map((response) => ({
 				status: response.status,
 				value: response.status === 'fulfilled' ? response.value : response.reason,
 			}))
