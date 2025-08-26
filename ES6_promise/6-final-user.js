83c23386c63588fa466f30f94b3c0c33c33dc25e
jeff <jeffrey-31@hotmail.fr>
2025-08-26 17:52:48 +0200

    for testing by checker before remake it

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,18 +4,18 @@ import uploadPhoto from "./5-photo-reject.js";
 export default function handleProfileSignup(firstName, lastName, filename) {
 	return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
 		.then(([responses]) => {
-			responses.map((resp) => {
-				if(resp.status === 'fulfilled') {
-				return {
-					status: resp.status,
-					value: resp.value,
-					}
-			} else {
-				return {
-					status: resp.status,
-					value: resp.reason,
-					}
-				}
-			})
+			return responses.map((resp) => {
+        if (resp.status === 'fulfilled') {
+          return {
+            status: resp.status,
+            value: resp.value,
+          };
+        } else {
+          return {
+            status: resp.status,
+            value: resp.reason,
+          };
+        }
+      });
 	})
 }
