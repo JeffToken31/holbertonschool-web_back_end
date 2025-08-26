01675fe1aa992aaf237cbb326e3067fbc77e6bed
jeff <jeffrey-31@hotmail.fr>
2025-08-26 13:57:57 +0200

    eslint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,8 @@
-export default function handleResponseFromAPI (promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
       status: 200,
-      body: 'Success'
+      body: 'Success',
     }
     ))
     .catch(() => new Error())
