039b6a8b55f998221229cd847c8e1909e555c71b
jeff <jeffrey-31@hotmail.fr>
2025-08-26 08:47:05 +0200

    code refatored

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,15 +1,10 @@
 export default function handleResponseFromAPI (promise) {
   return promise
-    .then(() => {
-      return {
+    .then(() => ({
         status: 200,
         body: 'Success'
-      };
-    })
-    .catch(() => {
-      return new Error();
-    })
-    .finally(() => {
-      console.log('Got a response from the API ');
-    });
+      }
+    ))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API '));
 }
