24c218e4b4a6ab5678d3c77326669b98d8ffb2c0
jeff <jeffrey-31@hotmail.fr>
2025-08-26 13:22:54 +0200

    delete space

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,9 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
     Promise.all([uploadPhoto(), createUser()])
       .then(([rep1, rep2]) =>
         console.log(`${rep1.body} ${rep2.firstName} ${rep2.lastName}`)
       )
-      .catch(() => new Error('Signup system offline'));
+      .catch(() => console.log('Signup system offline'));
 }
