ca3abceac695de0195ee6664930812e9f19167fc
jeff <jeffrey-31@hotmail.fr>
2025-09-01 11:37:48 +0200

    drop type controle and use console.log

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,8 +1,5 @@
 function displayMessage(message) {
-    if (typeof message !== "string") {
-        throw new Error('Must be a string');
-    }
-    process.stdout.write(message + '\n');
+    console.log(message);
 }
 
 module.exports = displayMessage;
