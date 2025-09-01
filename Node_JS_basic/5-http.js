6ea532d123bab799b0fc3b69e0f940a03c48be4e
jeff <jeffrey-31@hotmail.fr>
2025-09-01 20:47:42 +0200

    create basic http serve and manage eendpoint

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const { createServer } = require('node:http');
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+	if (req.url === ('/students')) {
+		countStudents(process.argv[2])
+      .then((data) => {
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((error) => {
+        res.end(error);
+      });
+	} else {
+		res.end('Hello Holberton School!');
+	}
+});
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
