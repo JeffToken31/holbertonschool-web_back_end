a00b3888c57c3f1b348f0df72347d096026ab14e
jeff <jeffrey-31@hotmail.fr>
2025-09-01 13:31:45 +0200

    ok checker do'nt love process.stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (name) => {
-  process.stdout.write('Your name is: ' + name);
-  process.stdout.write('This important software is now closing\n');
+  console.log(`Your name is: ${name.trim()}`);
+  console.log('This important software is now closing');
   process.exit();
 });
