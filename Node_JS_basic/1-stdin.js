be8db48c717e02752592509814233626b401d61c
jeff <jeffrey-31@hotmail.fr>
2025-09-01 13:28:49 +0200

    use process stdout instead of console.log

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (name) => {
-  console.log(`Your name is: ${name.trim()}`);
-  console.log('This important software is now closing');
+  process.stdout.write('Your name is: ' + name);
+  process.stdout.write('This important software is now closing\n');
   process.exit();
 });
