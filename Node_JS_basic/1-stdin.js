0cfca58767398bf6a6f50fcef1f388692c8d6c26
jeff <jeffrey-31@hotmail.fr>
2025-09-01 12:58:34 +0200

    Encode in utf8 and use trim to pop \n

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,8 @@
+process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (name) => {
-  console.log(`Your name is: ${name}`);
-	console.log('This important software is now closing');
+  console.log(`Your name is: ${name.trim()}`);
+  console.log('This important software is now closing');
   process.exit();
 });
