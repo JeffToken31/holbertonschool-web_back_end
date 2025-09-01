4a370751ffad0cc45394ce3551a6d6bb5c133065
jeff <jeffrey-31@hotmail.fr>
2025-09-01 16:47:48 +0200

    test with valid code

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,32 +1,44 @@
-const fs = require('node:fs');
+const fs = require('fs');
+
+const listName = (list) => list.map((element) => element[0]).join(', ');
 
 function countStudents(path) {
+  let data;
   try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    lines.shift();
-
-    console.log(`Number of students: ${lines.length}`);
-
-    const fields = {};
-    for (const line of lines) {
-      const [firstname, , , field] = line.split(',');
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-      fields[field].push(firstname);
-    }
-
-    for (const field of Object.keys(fields)) {
-      console.log(
-        `Number of students in ${field}: ${
-          fields[field].length
-        }. List: ${fields[field].join(', ')}`
-      );
-    }
+    data = fs.readFileSync(path).toLocaleString();
   } catch (err) {
     throw new Error('Cannot load the database');
   }
+  let start = true;
+  let nbStudent = 0;
+  const nbStudentCS = [];
+  const nbStudentSWE = [];
+  const rows = data.split('\n');
+
+  rows.forEach((row) => {
+    const columns = row.split(',');
+    if (!start && columns[0] !== '') {
+      if (columns[3] === 'CS') {
+        nbStudentCS.push(columns);
+      } else {
+        nbStudentSWE.push(columns);
+      }
+      nbStudent += 1;
+    }
+    start = false;
+  });
+  console.log(`Number of students: ${nbStudent}`);
+  console.log(
+    `Number of students in CS: ${nbStudentCS.length}. List: ${listName(
+      nbStudentCS
+    )}`
+  );
+  console.log(
+    `Number of students in SWE: ${nbStudentSWE.length}. List: ${listName(
+      nbStudentSWE
+    )}`
+  );
 }
 
+module.exports = listName;
 module.exports = countStudents;
