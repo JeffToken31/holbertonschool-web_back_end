78780b4cec0eadf863d5442640c532059d427ef8
jeff <jeffrey-31@hotmail.fr>
2025-09-02 10:33:27 +0200

    fix: back to my better code

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,16 +1,16 @@
-const http = require('http');
+const { createServer } = require('node:http');
 const countStudents = require('./3-read_file_async');
 
 const hostname = '127.0.0.1';
 const port = 1245;
-const app = http.createServer((req, res) => {
+const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === ('/students')) {
     countStudents(process.argv[2])
       .then((data) => {
-        res.write(`This is the list of our students\n${data.join('\n')}`);
-        res.end();
+        res.write('');
+        res.end(`This is the list of our students\n${data.join('\n')}`);
       })
       .catch((error) => {
         res.statusCode = 500;
