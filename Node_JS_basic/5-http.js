c5eb84246f62e5ff7479ba26bb882c410f2d87ac
jeff <jeffrey-31@hotmail.fr>
2025-09-02 12:34:15 +0200

    fix: recreate code from scratch

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,23 +1,21 @@
-const http = require('http');
+const { createServer } = require('node:http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
-  if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === ('/students')) {
     countStudents(process.argv[2])
-      .then((datas) => {
-        res.end(`This is the list of our students\n${datas}`);
+      .then((data) => {
+        res.end(`This is the list of our students\n${data}`);
       })
-      .catch((err) => {
-        res.end(err.message);
+      .catch((error) => {
+        res.end(error);
       });
   } else {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   }
 });
-
 app.listen(1245);
 
 module.exports = app;
