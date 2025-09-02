90fc088b830c70a185c822a275cc366a6c97b0a1
jeff <jeffrey-31@hotmail.fr>
2025-09-02 10:28:37 +0200

    fix: import http instead of create server

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,9 +1,9 @@
-const { createServer } = require('node:http');
+const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const hostname = '127.0.0.1';
 const port = 1245;
-const app = createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === ('/students')) {
