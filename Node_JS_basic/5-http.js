d0095ba03b7a1db091cf6d6090ea33f878baae67
jeff <jeffrey-31@hotmail.fr>
2025-09-03 09:07:48 +0200

    fix: add str in error return

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,18 +4,18 @@ const countStudents = require('./3-read_file_async');
 const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === ('/students')) {
+  if (req.url === '/students') {
     countStudents(process.argv[2])
       .then((data) => {
         res.end(`This is the list of our students\n${data}`);
       })
       .catch((error) => {
-        res.end(error.message);
+        res.end(`This is the list of our students\n${error.message}`);
       });
   } else {
     res.end('Hello Holberton School!');
   }
 });
-app.listen(1245);
 
+app.listen(1245);
 module.exports = app;
